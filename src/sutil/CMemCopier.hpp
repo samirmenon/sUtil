0cdaac95c6e703597dfd308039cd4e6552f97061
Samir <smenon@stanford.edu>
2011-07-18 01:19:32 -0700

    Indentation fixes

--- src/sutil/CMemCopier.hpp
+++ src/sutil/CMemCopier.hpp
@@ -35,76 +35,103 @@ sUtil. If not, see <http://www.gnu.org/licenses/>.
 namespace sutil
 {
 
-/** A template class that can memcpy its member data
- * to a set of output buffers, whenever required.
- */
-template <typename T>
-class CMemCopier
-{
-private:
-  /** Original data source which is copied to a buffer */
-  const T* mem_;
-
-  /** The buffers into which the data is copied. */
-  std::vector<T*> buf_;
-
-  /** Is initialized? */
-  bool initialized_;
-
-public:
-  CMemCopier();
-  virtual ~CMemCopier(){}
-
-  /** Initializes a mem copier "from" buffer */
-  bool init(const T* arg_mem);
-
-  /** Registers a "to" buffer with the mem copier.
-   * Whenever copy is called next, the memory will
-   * be copied into these buffer(s). */
-  bool addBuf(T* arg_buf);
-
-  /** Function to de-register an output buffer.
-   * @param arg_buf : The pointer to the buffer to be removed
-   * @returns : Removed element if found. Null else.
+  /** A template class that can memcpy its member data
+   * to a set of output buffers, whenever required.
    */
-  T* removeBuf(T* arg_buf);
-
-  /** Copies the memory into all the associated output
-   * buffers.
-   * @returns : true if succeeded, else false */
-  bool copy();
-};
-
-template <typename T>
-CMemCopier<T>::CMemCopier()
-{
-  mem_ = NULL;
-  initialized_ = false;
-}
-
-template <typename T>
-bool CMemCopier<T>::init(const T* arg_mem)
-{
-  if(NULL == arg_mem)
-  { return false; }
-  else
+  template <typename T>
+  class CMemCopier
   {
-    mem_ = arg_mem;
+  private:
+    /** Original data source which is copied to a buffer */
+    const T* mem_;
 
-    //Set init variable
-    if(buf_.size() > 0)
-    { initialized_ = true; }
+    /** The buffers into which the data is copied. */
+    std::vector<T*> buf_;
 
-    return true;
+    /** Is initialized? */
+    bool initialized_;
+
+  public:
+    CMemCopier();
+    virtual ~CMemCopier(){}
+
+    /** Initializes a mem copier "from" buffer */
+    bool init(const T* arg_mem);
+
+    /** Registers a "to" buffer with the mem copier.
+     * Whenever copy is called next, the memory will
+     * be copied into these buffer(s). */
+    bool addBuf(T* arg_buf);
+
+    /** Function to de-register an output buffer.
+     * @param arg_buf : The pointer to the buffer to be removed
+     * @returns : Removed element if found. Null else.
+     */
+    T* removeBuf(T* arg_buf);
+
+    /** Copies the memory into all the associated output
+     * buffers.
+     * @returns : true if succeeded, else false */
+    bool copy();
+  };
+
+  template <typename T>
+  CMemCopier<T>::CMemCopier()
+  {
+    mem_ = NULL;
+    initialized_ = false;
   }
-}
 
-template <typename T>
-bool CMemCopier<T>::addBuf(T* arg_buf)
-{
-  if(NULL!= arg_buf)
+  template <typename T>
+  bool CMemCopier<T>::init(const T* arg_mem)
   {
-    //Check if the buffer is already there.
+    if(NULL == arg_mem)
+    { return false; }
+    else
+    {
+      mem_ = arg_mem;
+
+      //Set init variable
+      if(buf_.size() > 0)
+      { initialized_ = true; }
+
+      return true;
+    }
+  }
+
+  template <typename T>
+  bool CMemCopier<T>::addBuf(T* arg_buf)
+  {
+    if(NULL!= arg_buf)
+    {
+      //Check if the buffer is already there.
+      typename std::vector<T*>::iterator it, ite;
+      ite = buf_.end();
+      for(it= buf_.begin(); it!=ite; ++it)
+      {
+        //In case the pointed to memory has been
+        //deallocated.
+        if(arg_buf == (*it))
+        {
+          return false;
+        }
+      }
+
+      //Add the buffer (new) to the vector
+      buf_.push_back(arg_buf);
+
+      if(NULL != mem_)
+      { initialized_ = true; }
+
+      return true;
+    }
+    else
+    { return false; }
+  }
+
+  template <typename T>
+  T* CMemCopier<T>::removeBuf(T* arg_buf)
+  {
     typename std::vector<T*>::iterator it, ite;
     ite = buf_.end();
     for(it= buf_.begin(); it!=ite; ++it)
@@ -113,67 +140,40 @@ bool CMemCopier<T>::addBuf(T* arg_buf)
       //deallocated.
       if(arg_buf == (*it))
       {
-        return false;
+        buf_.erase(it);
+
+        //Set init variable
+        if(buf_.size() <= 0)
+        { initialized_ = false; }
+
+        return arg_buf;
       }
     }
-
-    //Add the buffer (new) to the vector
-    buf_.push_back(arg_buf);
-
-    if(NULL != mem_)
-    { initialized_ = true; }
-
-    return true;
+    return NULL;
   }
-  else
-  { return false; }
-}
 
-template <typename T>
-T* CMemCopier<T>::removeBuf(T* arg_buf)
-{
-  typename std::vector<T*>::iterator it, ite;
-  ite = buf_.end();
-  for(it= buf_.begin(); it!=ite; ++it)
+  template <typename T>
+  bool CMemCopier<T>::copy()
   {
-    //In case the pointed to memory has been
-    //deallocated.
-    if(arg_buf == (*it))
+    if(!initialized_)
+    { return false; }
+    else
     {
-      buf_.erase(it);
-
-      //Set init variable
-      if(buf_.size() <= 0)
-      { initialized_ = false; }
-
-      return arg_buf;
+      bool flag = true;
+      typename std::vector<T*>::iterator it, ite;
+      ite = buf_.end();
+      for(it= buf_.begin(); it!=ite; ++it)
+      {
+        //In case the pointed to memory has been
+        //deallocated.
+        if(NULL == (*it))
+        { flag = false; continue; }
+        else
+        { memcpy((void*)(*it),mem_,sizeof(T));  }
+      }
+      return flag;
     }
   }
-  return NULL;
-}
-
-template <typename T>
-bool CMemCopier<T>::copy()
-{
-  if(!initialized_)
-  { return false; }
-  else
-  {
-    bool flag = true;
-    typename std::vector<T*>::iterator it, ite;
-    ite = buf_.end();
-    for(it= buf_.begin(); it!=ite; ++it)
-    {
-      //In case the pointed to memory has been
-      //deallocated.
-      if(NULL == (*it))
-      { flag = false; continue; }
-      else
-      { memcpy((void*)(*it),mem_,sizeof(T));  }
-    }
-    return flag;
-  }
-}
 
 
 }
--- src/sutil/CSingleton.hpp
+++ src/sutil/CSingleton.hpp
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU Lesser General Public
 License and a copy of the GNU General Public License along with
 this file. If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 /* \file CSingleton.hpp
  *
  *  Created on: Jul 8, 2011
