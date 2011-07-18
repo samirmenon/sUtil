7489d2a8d270543ee42ef5e9a6825a323b141939
Samir <smenon@stanford.edu>
2011-07-17 17:40:15 -0700

    Adding some more tests

--- applications-linux/sutil_test/test-singleton.hpp
+++ applications-linux/sutil_test/test-singleton.hpp
@@ -50,6 +50,15 @@ namespace sutil_test
     SSingletonTester() : bobo(1.0), lobo("lobo"){}
   };
 
+  /** Temporary struct to test the singleton */
+  struct SSingletonTester2
+  {
+    int x[11];
+    std::string bobo, lobo;
+
+    SSingletonTester2() : bobo("bobo"), lobo("lobo"){}
+  };
+
   /** Tests the Singleton implementation
    * @param arg_id : The id of the test */
   void test_singleton(const unsigned int arg_id)
@@ -61,16 +70,31 @@ namespace sutil_test
     {
       //Temporary typedef to refrain from typing this over and over
       typedef sutil::CSingleton<SSingletonTester> CDb;
-      SSingletonTester *db, *db2; //To store pointers from the singleton
+      SSingletonTester *db_a, *db_b; //To store pointers from the singleton
 
-      db = CDb::getData();
-      if(NULL == db) { throw(std::runtime_error("Singleton returned NULL after creation")); }
-      else  { std::cout<<"\nTest Result ("<<test_id++<<") Tested that singleton doesn't return NULL"; }
+      db_a = CDb::getData();
+      if(NULL == db_a) { throw(std::runtime_error("Singleton returned NULL after creation")); }
+      else  { std::cout<<"\nTest Result ("<<test_id++<<") Tested that singleton doesn't return NULL on data structure 1"; }
 
       //Double check singleton mechanism
-      db2 = CDb::getData();
-      if(db2 != db) { throw(std::runtime_error("Singleton failed: Singleton pointer changed after creation")); }
-      else  { std::cout<<"\nTest Result ("<<test_id++<<") Singleton pointer remains the same for multiple calls"; }
+      db_b = CDb::getData();
+      if(db_b != db_a) { throw(std::runtime_error("Singleton failed: Singleton pointer changed after creation")); }
+      else  { std::cout<<"\nTest Result ("<<test_id++<<") Singleton pointer remains the same for multiple calls on data structure 1"; }
+
+      //Stress check the singleton with a different data structure
+      typedef sutil::CSingleton<SSingletonTester2> CDb2;
+      SSingletonTester2 *db2_a, *db2_b; //To store pointers from the singleton
+      const unsigned int test_iters(1000);
+      for(unsigned int i=0; i<test_iters; ++i)
+      {
+        db2_a = CDb2::getData();
+        if(NULL == db2_a) { throw(std::runtime_error("Singleton returned NULL after creation on data structure 2")); }
+
+        //Double check singleton mechanism
+        db2_b = CDb2::getData();
+        if(db2_b != db2_a) { throw(std::runtime_error("Singleton failed: Singleton pointer changed after creation on data structure 2")); }
+      }
+      std::cout<<"\nTest Result ("<<test_id++<<") Singleton works for "<<test_iters<<" calls on data structure 2";
 
       std::cout<<"\nTest #"<<arg_id<<" (Singleton Test) Successful";
     }
