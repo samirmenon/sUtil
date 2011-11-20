0a04479ba8e83615cf9630f50cb498124e95ed4b
Samir <smenon@stanford.edu>
2011-11-20 03:22:03 -0800

    Finalizing obj history. Todo tests for it next.

--- src/sutil/CObjectHistory.hpp
+++ src/sutil/CObjectHistory.hpp
@@ -1,7 +1,6 @@
-/* This file is part of scl, a control and simulation library
-for robots and biomechanical models.
+/* This file is part of sutil, a random collection of utilities.
 
-scl is free software; you can redistribute it and/or
+sutil is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 3 of the License, or (at your option) any later version.
@@ -11,22 +10,20 @@ modify it under the terms of the GNU General Public License as
 published by the Free Software Foundation; either version 2 of
 the License, or (at your option) any later version.
 
-scl is distributed in the hope that it will be useful,
+sutil is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
 License and a copy of the GNU General Public License along with
-scl. If not, see <http://www.gnu.org/licenses/>.
+sutil. If not, see <http://www.gnu.org/licenses/>.
  */
 /* \file  CObjectHistory.hpp
  *
  *  Created on: Nov 18, 2011
  *
- *  Copyright (C) 2010
- *
- *  Author: Samir Menon <smenon@stanford.edu>
+ *  Copyright (C) 2011, Samir Menon <smenon@stanford.edu>
  */
 
 #ifndef COBJECTHISTORY_HPP_
@@ -35,163 +32,149 @@ scl. If not, see <http://www.gnu.org/licenses/>.
 #include <sutil/CMappedList.hpp>
 #include <sutil/CSystemClock.hpp>
 
+#ifdef DEBUG
+#include <iostream>
+#endif
+
 namespace sutil
 {
-  namespace cobjecthistory
-  { class CObjectHistoryBase;  }
-
-  /** This class ensures that multiple types can be inserted into the object
-   * store.
+  /** Time stamps an object, copies it and saves it.
+   * The object may be restored at any future point of time.
    *
-   * NOTE : You are responsible to make sure that this doesn't segfault!
-   */
-  template<typename StoredObjectType>
-  class CObjectHistory : public cobjecthistory::CObjectHistoryBase
+   * NOTE: Since this requires a memory copy, it will be
+   *       slow in general.
+   *
+   * Templpate parameters:
+   * 1. Idx :
+   * The class will create a map and will index all stored
+   * objects by an Idx type key. Each object's key must be
+   * unique.
+   *
+   * 2. StoreObjectsAs :
+   * The class requires a you to convert all objects
+   * into a given StoreObjectsAs type.
+   * The StoreObjectsAs type must have a public constructor,
+   * a copy constructor and a destructor that correctly
+   * deallocates its memory.
+   * Ie. An Idx will work, but a simple typecast to
+   * char* won't.
+   *
+   * Of course, you must use another class in tandem to convert
+   * your objects into something that can be stored here.
+   * */
+  template<typename Idx, typename StoreObjectsAs /* = Idx //works best */>
+  class CObjectHistory
   {
-  public:
+  protected:
+    /** Index all stored objects by an index and timestamp in a two
+     * level mapped list. */
+    CMappedList<
+      /** The specified index type */
+      Idx,
+      /** For each logged object, store a mapped list indexed
+       * by the timestamp at which the object was logged. */
+      CMappedList<double, StoreObjectsAs>
+    > data_;
+
     /** Saves the current state of an object (can be retrieved later) */
-    bool saveObject(const std::string& arg_name, const StoredObjectType& arg_obj)
+    bool saveObject(const Idx& arg_idx, const StoreObjectsAs& arg_obj)
     {
-      StoredObjectType* tobj = new StoredObjectType(arg_obj);
-      bool flag = saveObject(arg_name, reinterpret_cast<const void*>( tobj ));
-      if(false == flag)
+      bool flag
+      CMappedList<double, StoreObjectsAs>* objl = data_.at(arg_idx);
+      if(NULL == objl)
       {
-        delete tobj;
-#ifdef DEBUG
-            std::cout<<"\nCObjectHistoryBase::saveObject() : Could not create an entry for this object";
-#endif
-        return false;
-      }
-      return true;
-    }
-
-    /** Retrieves an object's entire stored history.
-     *
-     * NOTE : You are responsible to make sure that this doesn't segfault! */
-    const CMappedPointerList<double, StoredObjectType, false>*
-    getObjectTimeSeries(const std::string& arg_name)
-    { return reinterpret_cast<CMappedPointerList<double, StoredObjectType, false>*>(
-        CObjectHistoryBase::getObjectTimeSeries(arg_name)); }
-
-    /** Retrieves an object from a point in its stored history
-     *
-     * NOTE : You are responsible to make sure that this doesn't segfault! */
-    const void* getObject(const std::string& arg_name,
-        /** Optional timestamp argument. Ignore this to get the last saved state. */
-        double arg_time = -1.0)
-    { return reinterpret_cast<StoredObjectType*>(
-        STimeStampedObjects::getData()->data_.at(arg_name)); }
-
-    /** Pointer deletion requires knowing the type.
-     *
-     * NOTE : You are responsible to make sure that this doesn't segfault! */
-    virtual const bool removeObjectTimeSeries(const std::string& arg_name)=0;
-
-    /** Pointer deletion requires knowing the type.
-     *
-     * NOTE : You are responsible to make sure that this doesn't segfault! */
-    virtual const bool removeObject(const std::string& arg_name,
-        /** Optional timestamp argument. Ignore this to remove the last saved state. */
-        double arg_time = -1)=0;
-
-  private:
-    /** Private constructors : Use the static methods. */
-    CObjectHistoryBase(){}
-    virtual ~CObjectHistoryBase(){}
-    CObjectHistoryBase(const CObjectHistoryBase&){}
-    CObjectHistoryBase& operator =(const CObjectHistoryBase&){}
-  };
-
-  namespace cobjecthistory
-  {
-    /** Time stamps an object, copies it and saves it.
-     * The object may be restored at any future point of time. */
-    class CObjectHistoryBase
-    {
-    protected:
-      /** Index all stored objects by name and timestamp in a two
-       * level mapped list. */
-      class SObjectStore
-      {
-      public:
-        /** Each stored object can be accessed by name */
-        CMappedList<std::string,
-          /** For each logged object, store a mapped list indexed
-           * by the timestamp at which the object was logged. */
-          CMappedPointerList<double, void, false> > data_;
-      };
-
-      /** A data store of all stored and timestamped objects */
-      typedef CSingleton<SObjectStore> STimeStampedObjects;
-
-      /** Saves the current state of an object (can be retrieved later) */
-      bool saveObject(const std::string& arg_name, const void * arg_obj)
-      {
-        bool flag
-        CMappedPointerList<double, void>* pl;
-        pl = STimeStampedObjects::getData()->data_.at(arg_name);
-        if(NULL == pl)
-        {
-          flag = STimeStampedObjects::getData()->data_.create(arg_name);
-          if(false == flag)
-          {
-#ifdef DEBUG
-            std::cout<<"\nCObjectHistoryBase::addObject() : Could not create an entry for this object";
-#endif
-            return false;
-          }
-          pl = STimeStampedObjects::getData()->data_.at(arg_name);
-          if(NULL == pl)
-          {
-#ifdef DEBUG
-            std::cout<<"\nCObjectHistoryBase::addObject() : Created an entry for this object but can't access it. Invalid state.";
-#endif
-            return false;
-          }
-        }
-
-        flag = pl->create(CSystemClock::getSysTime(), arg_obj);
+        flag = data_.create(arg_idx);
         if(false == flag)
         {
 #ifdef DEBUG
-          std::cout<<"\nCObjectHistoryBase::addObject() : Could not timestamp and store this object";
+          std::cout<<"\nCObjectHistory::addObject() : Could not create an entry for this object";
 #endif
           return false;
         }
-
-        return true;
+        objl = data_.at(arg_idx);
+        if(NULL == objl)
+        {
+#ifdef DEBUG
+          std::cout<<"\nCObjectHistory::addObject() : Created an entry for this object but can't access it. Invalid state.";
+#endif
+          return false;
+        }
       }
 
-      /** Retrieves an object's entire stored history */
-      const CMappedPointerList<double, void, false>*
-      getObjectTimeSeries(const std::string& arg_name)
-      { return STimeStampedObjects::getData()->data_.at(arg_name); }
+      flag = objl->create(CSystemClock::getSysTime(), arg_obj);//Stores objects at the start
+      if(false == flag)
+      {
+#ifdef DEBUG
+        std::cout<<"\nCObjectHistory::addObject() : Could not timestamp and store this object";
+#endif
+        return false;
+      }
 
-      /** Retrieves an object from a point in its stored history */
-      const void* getObject(const std::string& arg_name,
-          /** Optional timestamp argument. Ignore this to get the last saved state. */
-          double arg_time = -1.0)
-      { return STimeStampedObjects::getData()->data_.at(arg_name); }
+      return true;
+    }
 
-      /** Pointer deletion requires knowing the type. Must be */
-      virtual const bool removeObjectTimeSeries(const std::string& arg_name)=0;
+    /** Retrieves an object's entire stored history */
+    const CMappedList<double, StoreObjectsAs>*
+    getObjectTimeSeries(const Idx& arg_idx)
+    { return data_.at(arg_idx); }
 
-      /** Pointer deletion requires knowing the type. */
-      virtual const bool removeObject(const std::string& arg_name,
-          /** Optional timestamp argument. Ignore this to remove the last saved state. */
-          double arg_time = -1)=0;
+    /** Retrieves an object from a point in its stored history */
+    const StoreObjectsAs* getObject(const Idx& arg_idx,
+        /** Optional timestamp argument. Ignore this to get the last saved state. */
+        double arg_time = -1.0)
+    {
+      CMappedList<double, StoreObjectsAs>* objl = data_.at(arg_idx);
+      if(NULL == objl)
+      {
+#ifdef DEBUG
+        std::cout<<"\nCObjectHistory::getObject() : Can't find object in history.";
+#endif
+        return NULL;
+      }
+      //Now check for the time
+      if(arg_time > 0.0)
+      { return objl->at(arg_idx); }
+      else
+      { return objl->at( std::size_t(0) );  }
+    }
 
-      /** Protected constructor : To ensure only subclasses may call it. */
-      CObjectHistoryBase(){}
+    /** Pointer deletion requires knowing the type. Must be */
+    const bool removeObjectTimeSeries(const Idx& arg_idx)
+    { return data_.erase(arg_idx); }
 
-    private:
-      /** Private constructor : To ensure uniqueness */
-      CObjectHistoryBase(const CObjectHistoryBase&){}
+    /** Pointer deletion requires knowing the type. */
+    const bool removeObject(const Idx& arg_idx,
+        /** Optional timestamp argument. Ignore this to remove the last saved state. */
+        double arg_time = -1)
+    {
+      CMappedList<double, StoreObjectsAs>* objl = data_.at(arg_idx);
+      if(NULL == objl)
+      {
+#ifdef DEBUG
+        std::cout<<"\nCObjectHistory::removeObject() : Can't find object in history.";
+#endif
+        return NULL;
+      }
+      //Now check for the time
+      if(arg_time > 0.0)
+      { return objl->erase(arg_idx); }
+      else
+      { return objl->erase( std::size_t(0) );  }
+    }
 
-      /** Private operator : To ensure uniqueness */
-      CObjectHistoryBase& operator =(const CObjectHistoryBase&){}
-    };
-  }
+    /** Constructor : Does nothing. */
+    CObjectHistory(){}
+
+    /** Destructor : Does nothing. */
+    ~CObjectHistory(){}
+
+  private:
+    /** Private constructor : To ensure uniqueness */
+    CObjectHistory(const CObjectHistory&){}
+
+    /** Private operator : To ensure uniqueness */
+    CObjectHistory& operator =(const CObjectHistory&){}
+  };
 
 } /* namespace sutil */
 #endif /* COBJECTHISTORY_HPP_ */
